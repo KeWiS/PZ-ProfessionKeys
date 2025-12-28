51fe32a5c7ea4955c50015b0e221c780418379e4
Michał Siwek <mjzsiwek@gmail.com>
2025-12-29 00:44:49 +0100

    chore: Added logging function

--- Contents/mods/ProfessionKeys/common/media/lua/shared/ProfessionKeysLogger.lua
+++ Contents/mods/ProfessionKeys/common/media/lua/shared/ProfessionKeysLogger.lua
@@ -1,5 +1,11 @@
 ProfessionKeysLogger = ProfessionKeysLogger or {}
 
+function ProfessionKeysLogger.logOnlyMessage(message)
+    if isDebugEnabled then
+        print("[ProfessionKeys] " .. message)
+    end
+end
+
 function ProfessionKeysLogger.log(buildingType, message)
     if isDebugEnabled then
         print("[ProfessionKeys] " .. buildingType .. ": " .. message)
