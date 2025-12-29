76d37262aed811b5ccf7faec3b053a056f6caf99
Michał Siwek <mjzsiwek@gmail.com>
2025-12-29 02:18:04 +0100

    fix: Fixed debug enabled check

--- Contents/mods/ProfessionKeys/common/media/lua/shared/ProfessionKeysLogger.lua
+++ Contents/mods/ProfessionKeys/common/media/lua/shared/ProfessionKeysLogger.lua
@@ -1,13 +1,13 @@
 ProfessionKeysLogger = ProfessionKeysLogger or {}
 
 function ProfessionKeysLogger.logOnlyMessage(message)
-    if isDebugEnabled then
+    if isDebugEnabled() then
         print("[ProfessionKeys] " .. message)
     end
 end
 
 function ProfessionKeysLogger.log(buildingType, message)
-    if isDebugEnabled then
+    if isDebugEnabled() then
         print("[ProfessionKeys] " .. buildingType .. ": " .. message)
     end
 end
